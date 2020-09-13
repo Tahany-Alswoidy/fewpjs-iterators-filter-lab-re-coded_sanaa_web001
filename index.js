25f3088ae288b6d11d57b2b9661de9ae549140cc
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 09:15:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function findMatching(drivers,matcher){
 function fuzzyMatch(drivers,matcher){
   return drivers.filter(driver=>(driver.substr(0,matcher.length).toLowerCase(0,matcher.length-1))===matcher.toLowerCase(matcher));
 }
+function matchName(drivers,matcher){
+  return drivers.filter(driver=>driver.name.toLowerCase(driver.name)===matcher.toLowerCase(matcher));
+}
